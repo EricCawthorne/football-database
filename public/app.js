60fb1a80efd4687034fa52c98c0c3bb1dc28c2c5
EricCawthorne <cawthorneeric@gmail.com>
2021-10-12 18:33:39 -0600

    cleaned up a few things

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
-document.getElementById('all stats').addEventListener('click', () => {
-    let id = $('#search').val()
+$('.search-button').on('click', () => {
+    let id = $('.search-bar').val()
     $.get(`http://localhost:3000/quarterbacks-with-stats/${id}`, (data) => {
         if (id.length === 0) {
             console.log(data)
